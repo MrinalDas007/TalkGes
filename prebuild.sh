1325d58eee04e7cd43a5c9f12f4863a5be24be24
MrinalDas007 <mrinaldas969@gmail.com>
2024-03-24 22:41:32 +0530

    Second Version

--- prebuild.sh
+++ prebuild.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-apt-get install -y gcc portaudio19-dev
-apt install python3-pyaudio
+apt-get install -y gcc portaudio19-dev python3-pyaudio
 pip install --no-cache-dir -r requirements.txt
