a329c03af8b2d586e76755465f2bbdc2db5d1f64
MrinalDas007 <mrinaldas969@gmail.com>
2024-03-24 22:11:25 +0530

    Second Version

--- prebuild.sh
+++ prebuild.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-# Gain root privileges
-sudo su
-
-# Remount the root file system as read-write
-mount -o remount,rw /
-
-# Update package lists and install system packages
-sudo apt-get update
-sudo apt-get install -y gcc portaudio19-dev
+apt-get update
+apt-get install -y gcc portaudio19-dev
+apt-get clean
+pip install --no-cache-dir -r requirements.txt
